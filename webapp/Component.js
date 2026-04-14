2b4fbe7dd26d6ff88be72bd3ffccd1c99f758c20
Yassine Samih <yassine.samih@gmx.de>
2026-04-14 07:14:44 +0000

    second commit

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,12 +1,31 @@
 sap.ui.define([
-    "sap/ui/core/UIComponent"
+    "sap/ui/core/UIComponent",
+    "sap/ui/Device"
 ],
-    function (UIComponent) {
+    function (UIComponent, Device) {
         "use strict";
 
         return UIComponent.extend("sap.training.exc.Component", {
 
+            metadata: {
+                manifest: "json"
+            },
 
+            init: function () {
+                // call the base component's init function
+                UIComponent.prototype.init.apply(this, arguments);
+            },
+
+            getContentDensityClass: function () {
+                if (!this._sContentDensityClass) {
+                    if (Device.support.touch) {
+                        this._sContentDensityClass = "sapUiSizeCozy";
+                    } else {
+                        this._sContentDensityClass = "sapUiSizeCompact";
+                    }
+                }
+                return this._sContentDensityClass;
+            }
 
 
         });
--- webapp/controller/Overview.controller.js
+++ webapp/controller/Overview.controller.js
@@ -1,14 +1,38 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/syncStyleClass",
+    "sap/ui/model/json/JSONModel"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, syncStyleClass, JSONModel) {
         "use strict";
 
         return Controller.extend("sap.training.exc.controller.Overview", {
 
+            onInit: function () {
+                var oModel = new JSONModel();
+                this.getView().setModel(oModel, "customer");
+            },
+
+            onSave: function () {
+                if (!this.pDialog) {
+                    this.pDialog = this.loadFragment({
+                        name: "sap.training.exc.view.Dialog"
+                    }).then(function (oDialog) {
+                        syncStyleClass(this.getOwnerComponent().getContentDensityClass(), this.getView(), oDialog);
+                        return oDialog;
+                    }.bind(this));
+                }
+                this.pDialog.then(function (oDialog) {
+                    oDialog.open();
+                });
+            },
+
+            onCloseDialog: function () {
+                this.byId("dialog").close();
+            }
 
         });
     });
